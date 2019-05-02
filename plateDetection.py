8cdfecffd65d4bf14fcee4c25a20653faa001aff
Bishoy Asaad <32459444+bishogasaad@users.noreply.github.com>
2019-05-02 17:37:56 +0200

    fixed image aspect ratio

--- plateDetection.py
+++ plateDetection.py
@@ -1,41 +1,40 @@
 import numpy as np
 import cv2
-
+from imutils import resize
 def detectedPlate(image,X,Y,W,H):
-    rect_image = image[Y : Y+H, X : X+W]
+    ratio = image.shape[1]/720
+    rect_image = image[int(Y*ratio): int((Y+H)*ratio), int(X*ratio): int((X+W)*ratio)]
     return rect_image
 
-cv2.namedWindow("output", cv2.WINDOW_NORMAL)       
-im = cv2.imread("car1.jpg")
+def detectplate(im):
 
-image = cv2.resize(im, (960, 540))
+    image = resize(im, 720)
 
-edges = cv2.Canny(image,100,200)
+    edges = cv2.Canny(image, 100, 300)
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-contours, hierarchy = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    height, width = edges.shape
+    imageContours = np.zeros((height, width, 3), dtype=np.uint8)
 
-height, width = edges.shape
-imageContours = np.zeros((height, width, 3), dtype=np.uint8)
+    cnts = []
 
-cnts = []
+    # Dimensions of the plate to be detected
+    X = 0
+    Y = 0
+    W = 0
+    H = 0
 
-# Dimensions of the plate to be detected
-X = 0
-Y = 0
-W = 0
-H = 0
+    # Filtered contours
+    for c in contours:
+        (x,y,w,h) = cv2.boundingRect(c)
 
-# Filtered contours
-for c in contours:
-    (x,y,w,h) = cv2.boundingRect(c)
-    if 3 > w/h > 2 and w*h > 6700:
-        cnts.append(c)
-        X = x
-        Y = y
-        W = w
-        H = h
-
-#cv2.rectangle(image, (X,Y), (X+W,Y+H), (0,255,0),2)
-rect_image = detectedPlate(image,X,Y,W,H)        
-cv2.imshow('',rect_image)
-cv2.waitKey(0) 
+        if 2.2 > w/h > 1.9 and w*h > 3000:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cnts.append(c)
+            X = x
+            Y = y
+            W = w
+            H = h
+    #cv2.rectangle(image, (X,Y), (X+W,Y+H), (0,255,0),2)
+    rect_image = detectedPlate(im,X,Y,W,H)
+    return rect_image
